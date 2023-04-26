5c0dd138332e8fd63166687892d3a3a7048f4266
rafacamarotti <110203753+rafacamarotti@users.noreply.github.com>
2023-04-25 21:27:52 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+ function preload(){
+    img= loadImage('dog_cat.jpg');
+ }
+ function setup(){
+    canvas = createCanvas(640, 420);
+    canvas.center();
+ }
+ function draw(){
+    image(img, 0, 0 ,640 ,420);
+    fill(255, 0, 0);
+    text("dog", 45, 75);
+    nofill();
+    stroke(255, 0, 0);
+    rect(30, 60, 450, 350);
+ }
