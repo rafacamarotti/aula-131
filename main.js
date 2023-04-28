98ab89e2c8978b0b9787f1407a2550b7ac65f3ae
rafacamarotti <110203753+rafacamarotti@users.noreply.github.com>
2023-04-27 21:28:53 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -4,6 +4,19 @@
  function setup(){
     canvas = createCanvas(640, 420);
     canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status: detectando objetos";
+ }
+ function modelLoaded(){
+   console.log("modelo carregado!")
+   status = true;
+   objectDetector.detect(img, gotResults);
+ }
+ function gotResults(error, results){
+   if(error){
+      console.log(error);
+   }
+   console.log(results);
  }
  function draw(){
     image(img, 0, 0 ,640 ,420);
@@ -12,4 +25,10 @@
     noFill();
     stroke(255, 0, 0);
     rect(30, 60, 450, 350);
+
+    fill("#FF0000");
+    text("cat", 320,120);
+    noFill();
+    stroke("FF0000");
+    rect(300, 90, 270, 320);
  }
