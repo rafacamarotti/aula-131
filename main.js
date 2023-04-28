1b3dcb65f9839499b82536ad3ebed3b931299f4e
rafacamarotti <110203753+rafacamarotti@users.noreply.github.com>
2023-04-28 20:28:05 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+ img = "";
+ objects=[];
+ status = "";
  function preload(){
     img= loadImage('dog_cat.jpg');
  }
@@ -17,18 +20,20 @@
       console.log(error);
    }
    console.log(results);
+   objects= results;
  }
  function draw(){
     image(img, 0, 0 ,640 ,420);
-    fill(255, 0, 0);
-    text("dog", 45, 75);
-    noFill();
-    stroke(255, 0, 0);
-    rect(30, 60, 450, 350);
+    if(status != ""){
+      for( i = 0;i< objects.length; i++){
+         document.getElementById("status").innerHTML = "status: objetos detectados";
 
-    fill("#FF0000");
-    text("cat", 320,120);
-    noFill();
-    stroke("#FF0000");
-    rect(300, 90, 270, 320);
+         fill("#FF0000");
+         percent= floor(objects[i].confidence * 100);
+         text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+         noFill();
+         stroke("#FF0000");
+         rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+      }
+    }
  }
